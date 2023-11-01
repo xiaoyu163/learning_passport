711af2ee51513f2c09edc1f15d60213b5b99db35
Lee Xiao Yu <u2005405@siswa.um.edu.my>
2023-11-02 00:43:26 +0800

    wsgi

--- learning_passport/wsgi.py
+++ learning_passport/wsgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_passport.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
