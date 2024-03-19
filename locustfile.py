5e18674894ba82e50724ba06467ff4bf3f2c1b55
Lee Xiao Yu <u2005405@siswa.um.edu.my>
2024-03-19 15:42:06 +0800

    Delete Locust

--- locustfile.py
+++
@@ -1,7 +0,0 @@
-from locustfile import HttpUser, task
-
-class HelloWorldUser(HttpUser):
-    @task
-    def login(self):
-        self.client.get("https://learning-passport-633550e98be1.herokuapp.com/login")
-        
